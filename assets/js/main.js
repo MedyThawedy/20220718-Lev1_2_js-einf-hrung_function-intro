36c19ec783268442663876880c92fd15dc163a4f
MD <dhmedy@googlemail.com>
2022-07-18 14:06:40 +0200

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 
 // Paramater
-function intro2(paramName) {
+function intro1(paramName) {
     let varName = 'SuperCoder';
-    console.log('Hi, ' + varName + '. Mein Name ist ' + paramName + ' .');
-    return ('Hi, ' + varName + '. Mein Name ist ' + paramName + ' .');
+    console.log('Hi, ' + varName + '. Mein Name ist ' + paramName + ' !');
+    return ('Hi, ' + varName + '. Mein Name ist ' + paramName + ' !');
 }
 
 
-let output1 = intro2('Slim Shady');
+let output1 = intro1('Slim Shady');
 output1 = '<p>' + output1 + '</p>';
 document.getElementById("withParam").innerHTML = output1;
 
@@ -16,9 +16,10 @@ document.getElementById("withParam").innerHTML = output1;
 // Argument
 function intro2() {
     let varName = 'SuperCoder';
+    // Argument value assignment
     let arg = arguments[0];
-    console.log('Hi, ' + varName + '. Mein Name ist ' + arg + ' .');
-    return ('Hi, ' + varName + '. Mein Name ist ' + arg + ' .');
+    console.log('Hi, ' + varName + '. Mein Name ist ' + arg + '.');
+    return ('Hi, ' + varName + '. Mein Name ist ' + arg + '.');
 }
 
 let output2 = intro2('Slim Shady');
